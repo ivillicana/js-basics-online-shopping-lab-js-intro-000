9a021ffcaef381783179ac29158baac479db814b
Kate Rentschler <nkpe@outlook.com>
2018-10-12 03:50:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function viewCart() {
   else if (getCart().length === 1){
     var itemNamesArray = Object.keys(getCart());
     var itemPricesArray = Object.values(getCart());
-    return `In your cart, you have ${itemNamesArray} at $${itemPricesArray}.`;
+    return `In your cart, you have ${itemNamesArray[0]} at $${itemPricesArray[0]}.`;
 
   }
 }
