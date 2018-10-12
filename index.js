dfe1915141c2ee64a07dea7194edb7922c67580b
Kate Rentschler <nkpe@outlook.com>
2018-10-12 02:16:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,31 @@ function addToCart(name) {
   getCart().push(items);
   return `${itemName} has been added to your cart.`;
 }
+
+function viewCart() {
+
+  if (getCart().length === 0){
+    return `Your shopping cart is empty.`;
+  }
+  
+  else if (getCart().length === 1){
+    return `In your cart, you have ${getCart().itemName} at $${getCart().itemPrice}, pancake batter at $5, and eggs at $49.`;
+  }
+
+  else if (getCart().length > 1){
+    return `In your cart, you have ${getCart()[i].first} at $${getCart().second}, pancake batter at $5, and eggs at $49.`;
+  }
+
+}
+
+function total() {
+  // write your code here
+}
+
+function removeFromCart(item) {
+  // write your code here
+}
+
+function placeOrder(cardNumber) {
+  // write your code here
+}
